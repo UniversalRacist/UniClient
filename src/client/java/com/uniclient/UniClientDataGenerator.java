01b3e837e9367f44365b0f25d11a520760dd9ac6
UniversalRacist <universalracist@github.com>
2026-01-04 00:11:57 +0530

    Add src files

---
+++ src/client/java/com/uniclient/UniClientDataGenerator.java
@@ -0,0 +1,11 @@
+package com.uniclient;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UniClientDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
